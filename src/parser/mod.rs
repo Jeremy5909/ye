19f17a7ac90e68306beee82a6d1ff096b0d11bab
Jeremy5909 <jeremyherczeg@gmail.com>
2025-05-08 08:37:20 -0700

    Seperate parser methods

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,4 +1,5 @@
 use crate::token::Token;
+mod parsing;
 
 #[derive(Debug)]
 pub enum Expr {
@@ -21,53 +22,5 @@ impl Parser {
     }
     fn peek(&self) -> Option<&Token> {
         self.tokens.get(self.index)
-    }
-    pub fn parse_expr(&mut self) -> Expr {
-        self.parse_term()
-    }
-    fn parse_term(&mut self) -> Expr {
-        let mut expr = self.parse_factor();
-        while let Some(tok) = self.peek() {
-            match tok {
-                Token::Add | Token::Sub => {
-                    let op = self.advance().unwrap().clone();
-                    let right = self.parse_factor();
-                    expr = Expr::Binary(Box::new(expr), op, Box::new(right));
-                }
-                _ => break,
-            }
-        }
-        expr
-    }
-    fn parse_factor(&mut self) -> Expr {
-        let mut expr = self.parse_primary();
-        while let Some(tok) = self.peek() {
-            match tok {
-                Token::Mult | Token::Div => {
-                    let op = self.advance().unwrap().clone();
-                    let right = self.parse_primary();
-                    expr = Expr::Binary(Box::new(expr), op, Box::new(right));
-                }
-                _ => break,
-            }
-        }
-        expr
-    }
-    fn parse_primary(&mut self) -> Expr {
-        if let Some(tok) = self.advance() {
-            match tok {
-                Token::Float(n) => Expr::Literal(*n),
-                Token::LParen => {
-                    let expr = self.parse_expr();
-                    match self.advance() {
-                        Some(Token::RParen) => expr,
-                        _ => panic!("Expected ')'"),
-                    }
-                }
-                _ => panic!("Unexpected token: {:?}", tok),
-            }
-        } else {
-            panic!("Unexpected end of input");
-        }
     }
 }
---
+++ src/parser/parsing.rs
@@ -0,0 +1,54 @@
+use crate::Parser;
+use crate::parser::Expr;
+use crate::token::Token;
+
+impl Parser {
+    pub fn parse_expr(&mut self) -> Expr {
+        self.parse_term()
+    }
+    fn parse_term(&mut self) -> Expr {
+        let mut expr = self.parse_factor();
+        while let Some(tok) = self.peek() {
+            match tok {
+                Token::Add | Token::Sub => {
+                    let op = self.advance().unwrap().clone();
+                    let right = self.parse_factor();
+                    expr = Expr::Binary(Box::new(expr), op, Box::new(right));
+                }
+                _ => break,
+            }
+        }
+        expr
+    }
+    fn parse_factor(&mut self) -> Expr {
+        let mut expr = self.parse_primary();
+        while let Some(tok) = self.peek() {
+            match tok {
+                Token::Mult | Token::Div => {
+                    let op = self.advance().unwrap().clone();
+                    let right = self.parse_primary();
+                    expr = Expr::Binary(Box::new(expr), op, Box::new(right));
+                }
+                _ => break,
+            }
+        }
+        expr
+    }
+    fn parse_primary(&mut self) -> Expr {
+        if let Some(tok) = self.advance() {
+            match tok {
+                Token::Float(n) => Expr::Literal(*n),
+                Token::LParen => {
+                    let expr = self.parse_expr();
+                    match self.advance() {
+                        Some(Token::RParen) => expr,
+                        _ => panic!("Expected ')'"),
+                    }
+                }
+                _ => panic!("Unexpected token: {:?}", tok),
+            }
+        } else {
+            panic!("Unexpected end of input");
+        }
+    }
+}
