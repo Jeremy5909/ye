34f336f7db7bb965e4c00eeb1c729b660ce4818f
Jeremy5909 <jeremyherczeg@gmail.com>
2025-05-15 15:45:59 -0700

    for loop now returns array of vals

--- src/eval/for_eval.rs
+++ src/eval/for_eval.rs
@@ -14,16 +14,22 @@ pub fn eval_for(
         Value::Array(arr) => arr,
         _ => return Err(ParsingError::ExpectedArray),
     };
-    let mut result = Value::Void;
+
+    let mut result = Vec::new();
     for item in elements {
         let mut loop_env = env.new_child();
         loop_env.set(&iter_name, item);
 
         for stmt in stmts {
             if let Some(val) = stmt.eval(&mut loop_env)? {
-                result = val;
+                match val {
+                    // should this typa thing be handeled earlier? should void even be a value type
+                    // or just option?
+                    Value::Void => (),
+                    _ => result.push(val),
+                }
             }
         }
     }
-    Ok(result)
+    Ok(Value::Array(result))
 }
